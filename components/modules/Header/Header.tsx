9b507eea4d44ec2d1cd2f05cc78b53213e569049
vsevolod <leto9999@gmail.com>
2024-03-21 18:59:18 +0300

    style header-top (logo)

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -1,6 +1,28 @@
+import styles from '../../../styles/header/header.module.scss'
+import LogoSVG from '@/components/elements/Logo/LogoSVG'
+
 const Header = () => (
-  <header className='header'>
-    <div className='container header__container'>header</div>
+  <header className={styles.header}>
+    <div className={styles.content}>
+      <div className={styles.top}>
+        <div className={styles.top}>
+          <div className={styles.logo}>
+            <LogoSVG />
+            <div className={styles.gallery}>
+              <p>Галерея молодых и</p>
+              <p>малоизвестных художников</p>
+              <p>Новое пространство</p>
+            </div>
+          </div>
+        </div>
+        <div>address</div>
+        <div>work time</div>
+        <div>???</div>
+        <div>Lang</div>
+      </div>
+      <div className={styles.middle}>Галерея молодых</div>
+      <div className={styles.bottom}>Меню</div>
+    </div>
   </header>
 )
 
