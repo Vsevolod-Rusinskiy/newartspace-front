f514c134923338febfa9db01aff6d3bec77ddef7
vsevolod <leto9999@gmail.com>
2024-03-21 09:47:55 +0300

    refactoring footer style

--- components/modules/Footer.tsx
+++ components/modules/Footer.tsx
@@ -1,34 +1,32 @@
 import styles from '../../styles/footer/index.module.scss'
-import cn from 'classnames'
-import { format } from 'date-fns'
 import Link from 'next/link'
+import { format } from 'date-fns'
 
 export const Footer = () => (
-  <footer className={styles.footer}>
+  <footer className={` ${styles.footer}`}>
     <div className={styles.content}>
-      <div className={styles.footer_menu}>
-        <nav className={cn(styles.nav_left, styles.menu_item)}>
+      <div className={styles.menu}>
+        <nav className={styles.nav}>
           <Link href='/catalog'>Каталог</Link>
           <Link href='/names'>Имена</Link>
           <Link href='/about'>О нас</Link>
           <Link href='/services'>Услуги</Link>
           <Link href='/another'>Другое</Link>
         </nav>
-        <nav className={cn(styles.nav_right, styles.menu_item)}>
+        <nav className={styles.nav}>
           <Link href='/events'>События</Link>
           <Link href='/contacts'>Контакты</Link>
           <Link href='#'>Условия возврата и доставки</Link>
           <a href='#'>Политика персональных данных</a>
           <a href='#'>Договор</a>
         </nav>
-        <address className={cn(styles.address, styles.menu_item)}>
+        <address className={styles.address}>
           <p>ИП Кабанченко Светлана Геннадьевна</p>
           <p>ИНН 781432217443</p>
           <p>
             Санкт - Петербург, ул. Ново-Рыбинская,
-            <br />
-            д. 19-21, БЦ «Квартал», центральный <br />
-            вход, 2 этаж, пом. 9
+            <br /> д. 19-21, БЦ «Квартал», центральный
+            <br /> вход, 2 этаж, пом. 9
           </p>
           <p>
             <a href='tel:89219326215'>Тел: +7 (921) 932-62-15</a>
