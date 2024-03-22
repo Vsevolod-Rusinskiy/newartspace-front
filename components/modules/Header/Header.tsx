24ad56d3aacb333434def27aaf99c0a7e4a6a83f
vsevolod <leto9999@gmail.com>
2024-03-22 14:45:12 +0300

    style header-top (work time)

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -5,14 +5,12 @@ const Header = () => (
   <header className={styles.header}>
     <div className={styles.content}>
       <div className={styles.top}>
-        <div className={styles.top}>
-          <div className={styles.logo}>
-            <LogoSVG />
-            <div className={styles.gallery}>
-              <p>Галерея молодых и</p>
-              <p>малоизвестных художников</p>
-              <p>Новое пространство</p>
-            </div>
+        <div className={styles.logo}>
+          <LogoSVG />
+          <div className={styles.gallery}>
+            <p>Галерея молодых и</p>
+            <p>малоизвестных художников</p>
+            <p>Новое пространство</p>
           </div>
         </div>
         <div className={styles.contacts}>
@@ -21,11 +19,16 @@ const Header = () => (
               9326215@mail.ru
             </a>
           </p>
-          <p className={styles.phone}>
-            <a href='tel:89219326215'>+7 (921) 932-62-15</a>
+          <p>
+            <a className={styles.phone} href='tel:89219326215'>
+              +7 (921) 932-62-15
+            </a>
           </p>
         </div>
-        <div>work time</div>
+        <div className={styles.work_time}>
+          <p>Пн – Пт с 13:00 до 19:00</p>
+          <p>Сб – Вс по предварительной договоренности</p>
+        </div>
         <div>???</div>
         <div>Lang</div>
       </div>
