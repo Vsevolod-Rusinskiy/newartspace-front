284a638a50a985c04f17ee2c7f8bfdfa99d81457
vsevolod <leto9999@gmail.com>
2024-06-19 14:24:47 +0300

    bfr env turning ...

--- next.config.mjs
+++ next.config.mjs
@@ -10,11 +10,6 @@ const nextConfig = {
         protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL,
         hostname: process.env.NEXT_PUBLIC_IMAGE_HOST,
         port: process.env.NEXT_PUBLIC_IMAGE_PORT,
-        ///---
-        // protocol: 'http',
-        // hostname: 'localhost',
-        // hostname: 'host.docker.internal',
-        // port: '3000',
       },
     ],
   },
