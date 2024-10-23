226bc7552d92ab59b4e8704bb962c0af56148eea
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-10-23 11:15:00 +0300

    feat: add modal

--- src/app/ui/layouts/Layout.tsx
+++ src/app/ui/layouts/Layout.tsx
@@ -1,16 +1,34 @@
+'use client'
 import { Header } from '@/src/widgets/Header'
 import { Footer } from '@/src/widgets/Footer'
 import { Sidebar } from '@/src/widgets/Sidebar'
+import { Modal } from '@/src/shared/ui/modals/Modal/Modal'
+import { useState } from 'react'
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-const Layout = ({ children }: { children: React.ReactNode }) => (
-  <>
-    <div>
-      <Header />
-      {children}
-      <Footer />
-    </div>
-    <Sidebar />
-  </>
-)
+  return (
+    <>
+      <div>
+        <button onClick={() => setIsModalOpen(true)}>toggle</button>
+        <Header />
+        {children}
+        <Footer />
+      </div>
+      <Sidebar />
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum
+      </Modal>
+    </>
+  )
+}
 
 export default Layout
---
+++ src/shared/ui/modals/Modal/Modal.tsx
@@ -0,0 +1,80 @@
+'use client'
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
+import styles from './Modal.module.scss'
+import cn from 'classnames'
+
+interface ModalProps {
+  className?: string
+  children?: ReactNode
+  isOpen?: boolean
+  onClose?: () => void
+}
+
+const ANIMATION_DELAY = 300
+
+export const Modal = (props: ModalProps) => {
+  const { children, isOpen, onClose } = props
+
+  const [isClosing, setIsClosing] = useState(false)
+  const timerRef = useRef<ReturnType<typeof setTimeout>>()
+
+  const closeHandler = useCallback(() => {
+    if (onClose) {
+      setIsClosing(true)
+      timerRef.current = setTimeout(() => {
+        onClose()
+        setIsClosing(false)
+      }, ANIMATION_DELAY)
+    }
+  }, [onClose])
+
+  // Новые ссылки!!!
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        closeHandler()
+      }
+    },
+    [closeHandler]
+  )
+
+  const onContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', onKeyDown)
+    }
+
+    return () => {
+      clearTimeout(timerRef.current)
+      window.removeEventListener('keydown', onKeyDown)
+    }
+  }, [isOpen, onKeyDown])
+
+  const mods: Record<string, boolean> = {
+    [styles.opened]: isOpen,
+    [styles.isClosing]: isClosing,
+  }
+
+  return (
+    // <Portal>
+    <div>
+      <div className={cn(styles.Modal, mods)}>
+        <div className={styles.overlay} onClick={closeHandler}>
+          <div className={styles.content} onClick={onContentClick}>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+    // </Portal>
+  )
+}
