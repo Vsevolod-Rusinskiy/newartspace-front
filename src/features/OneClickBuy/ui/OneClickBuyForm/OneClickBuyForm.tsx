1464d06e7929cae2c373a675e5e0f962e351bc8b
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-10-24 20:36:56 +0300

    features: add OneClickBuyForm and OneClickBuyModal

--- src/features/OneClickBuy/ui/OneClickBuyForm/OneClickBuyForm.tsx
+++ src/features/OneClickBuy/ui/OneClickBuyForm/OneClickBuyForm.tsx
@@ -1,15 +1,28 @@
 'use client'
 import Link from 'next/link'
 import { ActionButton } from '@/src/shared/ui/buttons/ActionButton/ActionButton'
-
+import { useRef, useEffect } from 'react'
+import { useAppSelector } from '@/src/app/model/redux/hooks'
 import './OneClickBuyForm.scss'
+
 export const OneClickBuyForm = () => {
-  const a = 1
-  console.log(a)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const isOpen = useAppSelector((state) => state.modalVisibility.isOpened)
+  useEffect(() => {
+    if (isOpen && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isOpen])
+
   return (
     <div className='form_container'>
       <span className='form_title'>Быстрый заказ</span>
-      <input className='form_input' type='text' placeholder='Имя*' />
+      <input
+        ref={inputRef}
+        className='form_input'
+        type='text'
+        placeholder='Имя*'
+      />
       <input className='form_input' type='tel' placeholder='Телефон*' />
       <input className='form_input' type='email' placeholder='Почта*' />
       <div className='form_checkbox_container'>
--- src/features/OneClickBuy/ui/OneClickBuyModal/OneClickBuyModal.tsx
+++ src/features/OneClickBuy/ui/OneClickBuyModal/OneClickBuyModal.tsx
@@ -1,12 +1,8 @@
 import { Modal } from '@/src/shared/ui/modals/Modal/Modal'
 import { OneClickBuyForm } from '../OneClickBuyForm/OneClickBuyForm'
 
-export const OneClickBuyModal = () => {
-  const a = 3
-  console.log(a)
-  return (
-    <Modal>
-      <OneClickBuyForm />
-    </Modal>
-  )
-}
+export const OneClickBuyModal = () => (
+  <Modal>
+    <OneClickBuyForm />
+  </Modal>
+)
