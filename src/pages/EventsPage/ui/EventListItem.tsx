fe6d20b30931a869129502ef29b78fcd06c43583
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-12-10 17:40:59 +0300

    chore: style events page

--- src/pages/EventsPage/ui/EventListItem.tsx
+++ src/pages/EventsPage/ui/EventListItem.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import { formatDateForRussia } from '@/src/shared/lib/common'
+import PageTextBlock from '@/src/shared/ui/PageTextBlock/PageTextBlock'
+import { DefaultButton } from '@/src/shared/ui/buttons/DefaultButton/DefaultButton'
+import cn from 'classnames'
 import styles from './EventListItem.module.scss'
+import Image from 'next/image'
 
 interface IEventListItem {
   id: number
@@ -14,11 +18,32 @@ export const EventListItem = ({
   date,
   content,
   imgUrl,
-}: IEventListItem) => (
-  <li className={styles.event_item}>
-    <h2 className={styles.event_title}>{title}</h2>
-    <p className={styles.event_date}>{date}</p>
-    <img src={imgUrl} alt={title} className={styles.event_img} />
-    <p className={styles.event_content}>{content}</p>
-  </li>
-)
+}: IEventListItem) => {
+  const maxDescriptionLength = 210
+
+  return (
+    <li className={styles.event_item}>
+      <h2 className={styles.event_title}>{title}</h2>
+      <p className={styles.event_date}>{formatDateForRussia(date)}</p>
+      <Image
+        src={imgUrl}
+        alt={title}
+        className={styles.event_img}
+        width={100}
+        height={100}
+        unoptimized
+      />
+      <PageTextBlock
+        text={
+          content.length > maxDescriptionLength
+            ? content.slice(0, maxDescriptionLength) + '. . .'
+            : content
+        }
+        className={styles.event_content}
+      />
+      <DefaultButton className={cn('action_button', styles.event_button)}>
+        ПОДРОБНЕЕ . . .
+      </DefaultButton>
+    </li>
+  )
+}
