361ac852e2dcc84426d242af9a2406bc18efbfaa
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-12-10 20:18:26 +0300

    feat: add canvas to events page

--- src/pages/EventsPage/ui/EventListItem.tsx
+++ src/pages/EventsPage/ui/EventListItem.tsx
@@ -4,6 +4,7 @@ import { DefaultButton } from '@/src/shared/ui/buttons/DefaultButton/DefaultButt
 import cn from 'classnames'
 import styles from './EventListItem.module.scss'
 import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 
 interface IEventListItem {
   id: number
@@ -21,20 +22,58 @@ export const EventListItem = ({
 }: IEventListItem) => {
   const maxDescriptionLength = 210
   const isVideo = imgUrl.endsWith('.mp4')
+  const [poster, setPoster] = useState<string | null>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    if (isVideo && videoRef.current && canvasRef.current) {
+      videoRef.current.addEventListener('loadeddata', () => {
+        videoRef.current.currentTime = 1 // Установите время, с которого хотите взять кадр
+      })
+
+      videoRef.current.addEventListener('seeked', () => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        if (context) {
+          context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height)
+          setPoster(canvas.toDataURL('image/png')) // Получаем изображение из canvas
+        }
+      })
+    }
+  }, [isVideo])
 
   return (
     <li className={styles.event_item}>
       <h2 className={styles.event_title}>{title}</h2>
       <p className={styles.event_date}>{formatDateForRussia(date)}</p>
       {isVideo ? (
-        <video
-          className={styles.event_img}
-          controls
-          poster={imgUrl.replace('.mp4', '.png')}
-        >
-          <source src={imgUrl} type='video/mp4' />
-          Ваш браузер не поддерживает видео.
-        </video>
+        <>
+          <video
+            ref={videoRef}
+            style={{ display: 'none' }}
+            src={imgUrl}
+            crossOrigin='anonymous'
+          />
+          <canvas
+            ref={canvasRef}
+            width={100}
+            height={100}
+            style={{ display: 'none' }}
+          />
+          {poster ? (
+            <Image
+              src={poster}
+              alt={title}
+              className={styles.event_img}
+              width={100}
+              height={100}
+              unoptimized
+            />
+          ) : (
+            <div>Загрузка обложки...</div>
+          )}
+        </>
       ) : (
         <Image
           src={imgUrl}
