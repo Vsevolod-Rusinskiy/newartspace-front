8fe3739323d3d2c88bb023bc2540861b54890387
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-12-14 13:22:01 +0300

    chore

--- src/pages/ServicesPage/ui/ServicesPage.tsx
+++ src/pages/ServicesPage/ui/ServicesPage.tsx
@@ -1,9 +1,6 @@
-import Bugsnag from '@bugsnag/js'
 import '../../temp/styles.css'
 
 export const ServicesPage = () => {
-  Bugsnag.notify(new Error('Test error for Bugsnag'))
-
   return (
     <div className='outerContainer'>
       <div className='innerContainer'>Страница в разработке . . .</div>
