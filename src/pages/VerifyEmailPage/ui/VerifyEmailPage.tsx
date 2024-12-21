ef9d99341301eafb0ac35842ac2f20a6cc61409f
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-12-21 09:39:51 +0300

    feat: add verification page and verify email

--- src/pages/VerifyEmailPage/ui/VerifyEmailPage.tsx
+++ src/pages/VerifyEmailPage/ui/VerifyEmailPage.tsx
@@ -1,9 +1,45 @@
-import '../../temp/styles.css'
+'use client'
 
-export const VerifyEmailPage = () => (
-  <div className='outerContainer'>
-    <div className='innerContainer'>
-      Страница верификации email в разработке . . .
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import axios from 'axios'
+import { API_BASE_URL } from '@/src/shared/config/apiConfig'
+import '../../temp/styles.css'
+import styles from './VerifyEmailPage.module.scss'
+
+export const VerifyEmailPage = () => {
+  const [verificationStatus, setVerificationStatus] = useState<string>('')
+  const searchParams = useSearchParams()
+  const token = searchParams.get('token')
+
+  useEffect(() => {
+    const verifyEmail = async () => {
+      console.log('Verification token:', token)
+
+      if (!token) {
+        setVerificationStatus('Токен верификации отсутствует')
+        return
+      }
+
+      try {
+        const response = await axios.get(
+          `${API_BASE_URL}/auth/verify-email?token=${token}`
+        )
+        console.log('Verification response:', response.data)
+      } catch (error) {
+        console.error('Verification error:', error)
+      }
+    }
+
+    verifyEmail()
+  }, [token])
+
+  return (
+    <div className='outerContainer'>
+      <div className={styles.verify_email_container}>
+        <h2 className={styles.verify_email_title}>Верификация Email</h2>
+        <p className={styles.verify_email_status}>{verificationStatus}</p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
