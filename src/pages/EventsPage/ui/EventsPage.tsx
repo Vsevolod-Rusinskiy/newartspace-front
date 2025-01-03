a2eeb3991b1155adef80fd1aa252b7c3f34e989e
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2025-01-03 14:49:47 +0300

    feat: enhance error handling and loading states in EventsPage and NamesPage
    - Added NoData component for improved user feedback when no data is available.
    - Refactored loading state handling to display skeleton loaders during data fetching.
    - Updated conditional rendering logic to handle error states and empty data scenarios.

--- src/pages/EventsPage/ui/EventsPage.tsx
+++ src/pages/EventsPage/ui/EventsPage.tsx
@@ -10,6 +10,7 @@ import { fetchEventsAction } from '../model/eventsPageSlice'
 import { RootState } from '@/src/app/model/redux/store'
 import { Paginate } from '@/src/shared/ui/Pagination/Pagination'
 import { Htag } from '@/src/shared/ui/Htag/Htag'
+import { NoData } from '@/src/shared/ui/NoData/NoData'
 import styles from './EventsPage.module.scss'
 import { EventListItem } from './EventListItem'
 
@@ -43,8 +44,6 @@ export const EventsPage = () => {
     }
   }, [loading])
 
-  if (error) return <div>Error: {error}</div>
-
   const eventArray = Array.isArray(events.data) ? events.data : []
 
   return (
@@ -53,26 +52,34 @@ export const EventsPage = () => {
         <div className={styles.title_container}>
           <Htag tag='h1'>События</Htag>
         </div>
-        <ul className={styles.event_list}>
-          {isLoading || isDelaying
-            ? Array.from({ length: 2 }).map((_, index) => (
-                <li key={index} className={styles.skeleton_list_item}>
-                  <div className={styles.skeleton_container}>
-                    <Skeleton className={styles.skeleton_item} />
-                  </div>
-                </li>
-              ))
-            : eventArray.map((event) => (
-                <EventListItem
-                  key={event.id}
-                  id={event.id}
-                  imgUrl={event.imgUrl}
-                  title={event.title}
-                  date={event.date}
-                  content={event.content}
-                />
-              ))}
-        </ul>
+        {error ? (
+          <NoData />
+        ) : isLoading || isDelaying ? (
+          <ul className={styles.event_list}>
+            {Array.from({ length: 2 }).map((_, index) => (
+              <li key={index} className={styles.skeleton_list_item}>
+                <div className={styles.skeleton_container}>
+                  <Skeleton className={styles.skeleton_item} />
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : events.data.length === 0 ? (
+          <NoData />
+        ) : (
+          <ul className={styles.event_list}>
+            {eventArray.map((event) => (
+              <EventListItem
+                key={event.id}
+                id={event.id}
+                imgUrl={event.imgUrl}
+                title={event.title}
+                date={event.date}
+                content={event.content}
+              />
+            ))}
+          </ul>
+        )}
         {events.data.length > 0 && (
           <Paginate
             pageCount={Math.ceil(events.total / limit)}
