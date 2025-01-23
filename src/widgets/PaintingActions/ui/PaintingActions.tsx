995334be85a5a52fc38f122d716ef6724da668ef
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2025-01-23 17:17:11 +0300

    refactor: simplify PaintingActions component by removing unused props and commented-out code; enhance button rendering logic based on priceType conditions for better user experience

--- src/widgets/PaintingActions/ui/PaintingActions.tsx
+++ src/widgets/PaintingActions/ui/PaintingActions.tsx
@@ -1,6 +1,5 @@
 import { useAppDispatch } from '@/src/app/model/redux/hooks'
 import { DefaultButton } from '@/src/shared/ui/buttons/DefaultButton/DefaultButton'
-import { actionOpenModal } from '@/src/shared/ui/modals/Modal/model/modalVisibilitySlice'
 import { toggleCart } from '@/src/entities/Cart/model/cartSlice'
 import { useRouter } from 'next/navigation'
 import { useSelector } from 'react-redux'
@@ -9,17 +8,15 @@ import cn from 'classnames'
 import styles from './PaintingActions.module.scss'
 
 interface PaintingActionsProps {
-  isReproducible?: boolean
-  priceType?: string
   className?: string
   paintingId?: number
+  priceType?: string
 }
 
 export const PaintingActions = ({
-  isReproducible,
-  priceType,
   className,
   paintingId,
+  priceType,
 }: PaintingActionsProps) => {
   const dispatch = useAppDispatch()
   const router = useRouter()
@@ -38,29 +35,35 @@ export const PaintingActions = ({
 
   return (
     <div className={cn(styles.actions, className)}>
-      <DefaultButton
+      {/* <DefaultButton
         className={cn('action_button', styles.action_button)}
         onClick={() => dispatch(actionOpenModal('ЗАКАЗАТЬ В ОДИН КЛИК'))}
         priceType={priceType}
       >
         ЗАКАЗАТЬ В ОДИН КЛИК
-      </DefaultButton>
-      {isReproducible && (
+      </DefaultButton> */}
+      {/* {isReproducible && (
         <DefaultButton
           className={cn('action_button', styles.action_button)}
           onClick={() => dispatch(actionOpenModal('ЗАКАЗАТЬ РЕПРОДУКЦИЮ'))}
         >
           ЗАКАЗАТЬ РЕПРОДУКЦИЮ
-        </DefaultButton>
-      )}
-      {paintingId && (
-        <DefaultButton
-          className={cn('action_button', styles.action_button)}
-          onClick={handleCartClick}
-        >
-          {isInCart ? 'ПЕРЕЙТИ В КОРЗИНУ' : 'ПОЛОЖИТЬ В КОРЗИНУ'}
-        </DefaultButton>
-      )}
+          </DefaultButton>
+        )} */}
+      {paintingId &&
+        /* eslint-disable indent */
+        priceType !== 'Возможна репродукция' &&
+        priceType !== 'Оригинал куплен' &&
+        priceType !== 'Оригинал не продаётся' &&
+        priceType !== 'Оригинал забронирован' && (
+          <DefaultButton
+            className={cn('action_button', styles.action_button)}
+            onClick={handleCartClick}
+          >
+            {isInCart ? 'ПЕРЕЙТИ В КОРЗИНУ' : 'ПОЛОЖИТЬ В КОРЗИНУ'}
+          </DefaultButton>
+        )}
     </div>
   )
+  /* eslint-enable indent */
 }
