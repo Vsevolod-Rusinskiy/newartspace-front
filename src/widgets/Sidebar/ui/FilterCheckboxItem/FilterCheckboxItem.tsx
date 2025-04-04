215b14e753d977cd83f9e68b7f92fb59906f6cd8
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2025-04-04 11:29:10 +0300

    feat: half Art types

--- src/widgets/Sidebar/ui/FilterCheckboxItem/FilterCheckboxItem.tsx
+++ src/widgets/Sidebar/ui/FilterCheckboxItem/FilterCheckboxItem.tsx
@@ -2,6 +2,7 @@ import { useAppDispatch } from '@/src/app/model/redux/hooks'
 import { actionCheckFilterItem } from '../../model/sideBarFiltersSlice'
 import { FilterItem } from '../../model/types'
 import styles from './FilterCheckboxItem.module.scss'
+import { useLang } from '@/src/shared/hooks/useLang'
 
 interface FilterCheckboxItemProps {
   filterList: FilterItem[]
@@ -13,9 +14,28 @@ const FilterCheckboxItem = ({
   filterName,
 }: FilterCheckboxItemProps) => {
   const dispatch = useAppDispatch()
+  const { lang, translations } = useLang()
+
   const handleCheck = (id: number, filterName: string) => {
     dispatch(actionCheckFilterItem({ id, filterName, isRadioButton: false }))
   }
+
+  // Функция для получения переведенного значения фильтра
+  const getTranslatedValue = (originalValue: string) => {
+    // Применяем перевод только для artTypesList
+    if (
+      filterName === 'artTypesList' &&
+      translations[lang].sidebar_filters.art_types_values
+    ) {
+      const artTypesValues = translations[lang].sidebar_filters
+        .art_types_values as Record<string, string>
+
+      return artTypesValues[originalValue] || originalValue
+    }
+    // Для всех остальных фильтров возвращаем оригинальное значение
+    return originalValue
+  }
+
   return (
     <ul className={styles.filter_list}>
       {filterList.map((filterItem) => (
@@ -28,7 +48,9 @@ const FilterCheckboxItem = ({
               className={styles.checkbox_small}
               onChange={() => handleCheck(filterItem.id, filterName)}
             />
-            <span className={styles.checkbox_label}>{filterItem.value}</span>
+            <span className={styles.checkbox_label}>
+              {getTranslatedValue(filterItem.value)}
+            </span>
           </label>
         </li>
       ))}
