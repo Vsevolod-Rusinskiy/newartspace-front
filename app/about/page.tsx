b16a5916dfc54d3dbaa7bbc7fa3d28353d3f955c
Vsevolod-Rusinskiy <leto99999@gmail.com>
2025-04-25 12:15:51 +0300

    feat: Add metadata for AboutPage to enhance SEO and improve page rendering

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,24 @@
 import { AboutPage } from '@/src/pages/AboutPage'
 
-export default async function Page() {
+export const metadata = {
+  title: 'О нас | Галерея молодых и малоизвестных художников',
+  description: 'Узнайте больше о нашей галерее, миссии и команде.',
+  openGraph: {
+    title: 'О нас | Галерея молодых и малоизвестных художников',
+    description: 'Узнайте больше о нашей галерее, миссии и команде.',
+    url: 'https://newartspace.ru/about',
+    type: 'website',
+    images: [
+      {
+        url: 'https://newartspace.ru/img/roses.png',
+        width: 1200,
+        height: 630,
+        alt: 'О нас | Галерея молодых и малоизвестных художников',
+      },
+    ],
+  },
+}
+
+export default function Page() {
   return <AboutPage />
 }
