1dad140b3004d29cce1c26034a2360f6b1f612b1
Vsevolod-Rusinskiy <leto99999@gmail.com>
2025-05-14 10:41:11 +0300

    chore: Temporarily disable Bugsnag integration in bugsnag.js for maintenance

--- src/shared/lib/bugsnag.js
+++ src/shared/lib/bugsnag.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import Bugsnag from '@bugsnag/js'
-import BugsnagPluginReact from '@bugsnag/plugin-react'
-import BugsnagPerformance from '@bugsnag/browser-performance'
+// import Bugsnag from '@bugsnag/js'
+// import BugsnagPluginReact from '@bugsnag/plugin-react'
+// import BugsnagPerformance from '@bugsnag/browser-performance'
 
 let ErrorBoundary = ({ children }) => <>{children}</>
 
-if (typeof window !== 'undefined') {
-  Bugsnag.start({
-    apiKey: '622519d9fb58dc777705ab4131cb670b',
-    plugins: [new BugsnagPluginReact()],
-  })
+// Временно отключаем Bugsnag
+// if (typeof window !== 'undefined') {
+//   Bugsnag.start({
+//     apiKey: '622519d9fb58dc777705ab4131cb670b',
+//     plugins: [new BugsnagPluginReact()],
+//   })
 
-  BugsnagPerformance.start({ apiKey: '622519d9fb58dc777705ab4131cb670b' })
+//   BugsnagPerformance.start({ apiKey: '622519d9fb58dc777705ab4131cb670b' })
 
-  ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
-}
+//   ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
+// }
 
 export { ErrorBoundary }
